e51cf48850a0335ca12495066bbd72da4bb51186
Mohammad Nassar <mohammad.nassar5@gmail.com>
2025-04-26 00:32:22 +0300

    Add app icon and fix google login.
    Signed-off-by: Mohammad Nassar <mohammad.nassar5@gmail.com>

--- android/app/src/main/kotlin/com/example/map_explorer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.najmshiel.map
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/najmshiel/map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.najmshiel.map
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity()
